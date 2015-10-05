68277e591c2d64323f4360848024100bfa4c6244
faresBelmadi <fares.belmadi@gmail.com>
2015-10-05 21:26:38 +0200

    modif return

--- Joueur.py
+++ Joueur.py
@@ -10,7 +10,7 @@ class Joueur:
 
     def viserUneCase(self):
         case=input("choisir une case")
-        colonne = ord(lower(case[0])-65) 
+        colonne = ord(lower(case[0])-65)
         ligne = case[1:]
         caseVisee=(ligne,colonne)
         self.grilleVisee.setVisee(caseVIsee)
@@ -23,8 +23,10 @@ class Joueur:
     def estTouche(self, caseVisee):
         for bateau in self.bateaux:
             if bateau.contient(caseVisee):
-                return True
-        return False
+                self.touche = True
+            else:
+                self.touche = False
+        return self.touche
 
     def notifierCaseTouchee(self,caseVisee):
         self.grilleTerrain.setTouchee(caseVisee)
